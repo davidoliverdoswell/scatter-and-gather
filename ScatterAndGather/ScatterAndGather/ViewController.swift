2a5575147f238b8de7cf86735f9ad2aa71e7de8e
Lambda-School-Loaner-11 <lambda-school-loaner-11@Lambda-School-Loaner-11s-MacBook-Pro.local>
2018-08-29 12:43:45 -0700

    Create a bar button that calls back to your main view controller

---
+++ ScatterAndGather/ScatterAndGather/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ScatterAndGather
+//
+//  Created by Lambda-School-Loaner-11 on 8/29/18.
+//  Copyright © 2018 David Doswell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var shouldScramble: Bool?
+    
+    @IBAction func toggle(_ sender: Any) {
+    
+    }
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+}
+
